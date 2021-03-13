d504c6f9cc4c08100dd441847220e65fd6ee24c3
lalvarezv1 <l.alvarezv1@uniandes.edu.co>
2021-03-13 18:13:16 -0500

    use connection pool

--- front/db.js
+++ front/db.js
@@ -1,17 +1,10 @@
 // Defines de connection to de DB
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
   host     : 'localhost',
   user     : 'user_taller1',
   password : 'taller1.',
   database : 'taller1'
-});
-connection.connect(function(err){
-if(!err) {
-    console.log("Base de datos conectada");
-} else {
-    console.log("No puedo conectarme a la BD");
-}
 });
 
 module.exports = connection; 
