cb7c26f5873eec4c32b44f9a813dccf8e09eb3de
lalvarezv1 <l.alvarezv1@uniandes.edu.co>
2021-03-17 09:49:26 -0500

    remove duplicates

--- sql/6_getRecomendationArtists.sql
+++ sql/6_getRecomendationArtists.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS `getRecomendationArtists`;
 CREATE PROCEDURE `getRecomendationArtists`(IN par_user_id int(11),in par_model varchar(15),in par_type_model varchar(15), IN par_limit int(11))
 BEGIN
-     SELECT ra.artist_name,
+     SELECT  DISTINCT ra.artist_name,
             ra.recomendation_score 
      FROM recomendations_artists AS ra
      WHERE ra.user_id = par_user_id
--- sql/7_getRecomendationTracks.sql
+++ sql/7_getRecomendationTracks.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS `getRecomendationTracks`;
 CREATE PROCEDURE `getRecomendationTracks`(IN par_user_id int(11),in par_model varchar(15),in par_type_model varchar(15), IN par_limit int(11))
 BEGIN
-     SELECT ra.track_name,
+     SELECT DISTINCT ra.track_name,
             ra.recomendation_score 
      FROM recomendations_tracks AS ra
      WHERE ra.user_id = par_user_id
